ef109c9b85053fa0e16f078ed18b0a02eb74b192
kranthi-77 <kranthivelanati@gmail.com>
2024-08-26 03:09:11 +0530

    commit

---
+++ src/components/Matrix.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Matrix.css";
+
+const Matrix = () => {
+  const [matrix, setMatrix] = useState(Array(9).fill(null));
+  const [clickOrder, setClickOrder] = useState([]);
+
+  const handleClick = (index) => {
+    if (matrix[index] === null) {
+      const newMatrix = [...matrix];
+      newMatrix[index] = "green";
+      setMatrix(newMatrix);
+      setClickOrder([...clickOrder, index]);
+
+      if (clickOrder.length === 8) {
+        setTimeout(() => {
+          const newMatrixOrange = Array(9).fill("orange");
+          setMatrix(newMatrixOrange);
+        }, 1000);
+      }
+    }
+  };
+
+  return (
+    <div className="matrix">
+      {matrix.map((color, index) => (
+        <div key={index} className="box" style={{ backgroundColor: color }} onClick={() => handleClick(index)} />
+      ))}
+    </div>
+  );
+};
+
+export default Matrix;
